84cf3cec9f0855ea98358f329c8c0b7981c5e0a9
Anni Hillig <a.hillig@opwoco.com>
2023-06-15 15:37:37 +0200

    fixed crash in recurring consumptions when in standby

--- app/src/main/java/eu/inscico/aurora_app/ui/components/recurringConsumptions/FrequencySpinner.kt
+++ app/src/main/java/eu/inscico/aurora_app/ui/components/recurringConsumptions/FrequencySpinner.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,7 +33,7 @@ fun FrequencySpinner(
 
     val context = LocalContext.current
 
-    val intervalUnit = rememberSaveable {
+    val intervalUnit = remember {
         mutableStateOf(initialValues?.unit ?: RecurringConsumptionIntervalUnit.DAILY)
     }
 
@@ -47,13 +48,13 @@ fun FrequencySpinner(
         }
     }
     val weekdayEntries =
-        rememberSaveable { mutableStateOf<List<MultiSelectEntry<*>>?>(updatedWeekdays) }
+        remember { mutableStateOf<List<MultiSelectEntry<*>>?>(updatedWeekdays) }
 
-    val selectedWeekdays = rememberSaveable {
+    val selectedWeekdays = remember {
         mutableStateOf(weekdayEntries.value?.filter { it.isSelected })
     }
 
-    val unitMonthlyDay = rememberSaveable {
+    val unitMonthlyDay = remember {
         mutableStateOf<Int?>(initialValues?.dayOfMonth)
     }
 
